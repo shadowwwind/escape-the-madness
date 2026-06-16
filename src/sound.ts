b7e1d92839f0477eba1bc7098f285e19353275f8
momo <you@example.com>
2026-06-16 14:35:00 +0200

    add sound toggle script

---
+++ src/sound.ts
@@ -0,0 +1,21 @@
+let soundOn:boolean = true;
+
+const soundButtons = document.querySelectorAll<HTMLButtonElement>("button.soundToggle");
+for (const soundButton of soundButtons) {
+    soundButton.addEventListener("click", toggleSounds)
+}
+function toggleSounds():boolean {
+    soundOn = !soundOn;
+
+    if (soundOn) {
+        for (const soundButton of soundButtons) {
+            soundButton.innerText = "Sound ist an"
+        }
+    }else {
+        for (const soundButton of soundButtons) {
+            soundButton.innerText = "Sound ist aus"
+        }
+    }
+
+    return soundOn;
+}
